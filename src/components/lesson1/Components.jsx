607c72bb55e7709d27ad2716d3e45d16f5daa1cb
Igor Storchevoy <hellsgor@gmail.com>
2025-01-24 15:19:10 +0400

    lesson 2

--- src/components/lesson1/Components.jsx
+++ src/components/lesson1/Components.jsx
@@ -1,7 +1,7 @@
-import { useState } from "react";
-import { list } from "./stubs";
-import { slowFunction } from "./utils";
-import "../styles.css";
+import { useEffect, useState } from 'react';
+import { list } from './stubs';
+import { slowFunction } from './utils';
+import '../styles.css';
 
 const ComponentA = () => {
   /* Устанавливаем стейт счетчика */
@@ -26,12 +26,19 @@ const ComponentA = () => {
 /** Пример тяжелых вычислений */
 const ComponentB = ({ count }) => {
   /* Ф-я, выполняющая достаточно трудоемкий код */
+  const startTime = performance.now();
   const result = slowFunction();
+  const endTime = performance.now();
+
+  useEffect(() => {
+    console.count('Render');
+  });
 
   return (
     <div className="component">
       <h3>Component B</h3>
       <p>{`Show result value: ${result}`}</p>
+      <p>{`Врем выполнения операции: ${Math.round(endTime - startTime)}`}</p>
     </div>
   );
 };
